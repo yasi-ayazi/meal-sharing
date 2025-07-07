63cdb4b7f0324a89720e717adef20f086fb0a311
Yasi Ayazi <yasi.ayazi@gmail.com>
2025-07-07 14:12:27 +0200

    Enhance mealsRouter to include additional sorting options and enrich meal data with available spots; refactor HomePage component for improved readability.

--- app-next/components/HomePage/HomePage.jsx
+++ app-next/components/HomePage/HomePage.jsx
@@ -3,20 +3,21 @@ import { MealsList } from "../MealsList/MealsList";
 import Link from "next/link";
 
 const HomePage = () => {
-    return (
-           <div style={{ padding: "2rem" }}>
-            <h1>Welcome to Meal Sharing!</h1>
-            <p>Find delicious meals shared by others.</p>
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Welcome to Meal Sharing!</h1>
+      <p>Find delicious meals shared by others.</p>
 
-            <MealsList />
+      
+      <MealsList limit={10} />
 
-            <div style={{ marginTop: "2rem" }}>
-                <Link href="/meals">
-                    <button>See all meals</button>
-                </Link>
-            </div>
-        </div>
-    );
+      <div style={{ marginTop: "2rem" }}>
+        <Link href="/meals">
+          <button>See all meals</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
